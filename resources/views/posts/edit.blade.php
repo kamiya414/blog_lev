921ca3f822f6a52f731cddc28fedb9fefad0b907
taichi-hrgch <taichi1115.blue@gmail.com>
2024-03-16 03:09:48 +0900

    add update for article

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{str_replace('_', '_', app()->getLocale())}}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="content_title">
+                <h2>タイトル</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="content_body">
+                <h2>本文</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+            <input type="submit" value="更新"/>
+        </form>
+        <div class='footer'>
+            <a href="/posts/{{ $post->id }}">戻る</a>
+        </div>
+    </body>
+</html>
+
