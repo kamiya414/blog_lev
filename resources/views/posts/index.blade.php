9af4a0cfbedcbe2c50183a4af36f8b9061116ea9
taichi-hrgch <taichi1115.blue@gmail.com>
2024-03-25 10:15:56 +0900

    add 認証

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,40 +6,46 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>Blog Name</h1>
-        <a href='/posts/create'>[create]</a>
-        <div class='post'>
-            @foreach($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                    <!-- actionにはweb.phpで定義したURLを記載　-->
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <!--Javascriptで削除 -->
-                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-        <!-- Javascript この位置に書くことで先にCSSを読み込み，速度上がる -->
-        <script>
-            function deletePost(id){
-                'use strick'
-                
-                if(confirm('削除すると復元できません．\n本当に削除しますか？')){
-                    document.getElementById(`form_${id}`).submit();
+    <x-app-layout>
+        <x-slot name="header">
+            index
+        </x-slot>
+        <body>
+            <h1>Blog Name</h1>
+            <a href='/posts/create'>[create]</a>
+            <div class='post'>
+                @foreach($posts as $post)
+                    <div class='post'>
+                        <h2 class='title'>
+                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                        <p class='body'>{{ $post->body }}</p>
+                        <!-- actionにはweb.phpで定義したURLを記載　-->
+                        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <!--Javascriptで削除 -->
+                            <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                        </form>
+                    </div>
+                @endforeach
+            </div>
+            <div class='paginate'>
+                {{ $posts->links() }}
+            </div>
+            <!-- Javascript この位置に書くことで先にCSSを読み込み，速度上がる -->
+            <script>
+                function deletePost(id){
+                    'use strick'
+                    
+                    if(confirm('削除すると復元できません．\n本当に削除しますか？')){
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+            <p>{{ Auth::user()->name }}</p>
+        </body>
+    </x-app-layout>
 </html>
 
